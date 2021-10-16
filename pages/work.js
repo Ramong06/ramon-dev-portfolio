fbe34d3e433710354b647593ab9fb64a3d04387d
RG-Mac <ramong5406@gmail.com>
2021-10-16 18:39:36 -0500

    first commit to repo

---
+++ pages/work.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Work = () => {
+    return (
+        <div>
+            <h1>This is the ABOUT me page</h1>
+            <Link href='/'>Back to Home</Link>
+        </div>
+    )
+}
+
+export default Work;
