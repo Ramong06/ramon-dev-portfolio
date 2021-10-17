fff771cf882b86eb0204f3e942c54e00ab5a43e0
RG-Mac <ramong5406@gmail.com>
2021-10-16 22:46:57 -0500

    more editing on navbar

--- pages/work.js
+++ pages/work.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
+import Nav from '../components/Nav';
 
 const Work = () => {
     return (
         <div>
+            <Nav />
             <h1>This is the ABOUT me page</h1>
             <Link href='/'>Back to Home</Link>
         </div>
